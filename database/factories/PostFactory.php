40e542bd65ee4d81168d5fea61642ea331972255
KhaledMohamed08 <khaledmohamed0896@gmail.com>
2025-08-06 12:46:47 +0300

    create Post model, migration, seeder and factory for generate dummy posts data.

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::inRandomOrder()->value('id'),
+            'title' => fake()->title(),
+            'description' => fake()->paragraph(),
+            'contact_phone_number' => fake()->e164PhoneNumber(),
+        ];
+    }
+}
